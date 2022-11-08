5ec317074a77c1ea28d2320f93f453bcfbe619c6
tomess00 <boube.sadik@gmail.com>
2022-11-08 03:52:15 +0100

    2e

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const navbar = document.querySelector('.navbar');
 const menuIcon = document.querySelector('.menu-toggle');
 const closeIcon = document.querySelector('.close-icon');
+const body = document.querySelector('body');
 
 
 // FONCTION QUI AFFICHE LA BARRE LATERALE
 function displaySidebar() {
    menuIcon.addEventListener('click', () => {
       navbar.classList.add('nav-active');
-      menuIcon.style.display = 'none';
+      body.style.overflow = 'hidden';
    })
 
    closeIcon.addEventListener('click', () => {
       navbar.classList.remove('nav-active');
       menuIcon.style.display = 'block';
+      body.style.overflow = 'visible';
    })
 }
 displaySidebar();
+
